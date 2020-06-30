ae4480b6f2bf56ecc594448bb41baef520515a0e
Dennis Mayr <dmayr.dev@gmail.com>
2020-06-30 18:07:41 -0400

    LibrosEscolares: 'formateaPrecio' => 'precioFormateado', imprimir() completo

--- src/Main.kt
+++ src/Main.kt
@@ -16,12 +16,21 @@ fun main() {
 }
 
 // 3.
-fun formateaPrecio(precioLibro: Int): String {
+fun precioFormateado(precioLibro: Int): String { // argumento nuevo (precioLibro de tipo Int), parseado a String
     return "$${precioLibro}"
 // al llamar a este metodo, se puede pasar e.g. libro1.precio,
 }
 
 // 4.
-//fun LibrosEscolares.imprimir():String {
-//return this.nombreDeLibro, this.
+fun imprimir(libro: LibrosEscolares) { // por cada `libros` en `LibrosEscolares`
+    println(
+        "Nombre: ${libro.nombreDeLibro}\n" +
+                "ISBN: ${libro.ISBN}\n" +
+                "Año de publicación: ${libro.anioPublicacion}\n" +
+                "Editorial: ${libro.editorial}\n" +
+                "Número de páginas: ${libro.cantPaginas}\n" +
+                "Precio: ${precioFormateado(libro.precio)}\n" +
+                "Autor: ${libro.autorLibro}\n" +
+                "Formato: ${libro.formatoDeLibro}\n"
+    )
 }
