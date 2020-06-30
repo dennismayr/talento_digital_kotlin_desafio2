be8953cf7a3b1ff1eb56e32d8585d0281542ccd1
Dennis Mayr <dmayr.dev@gmail.com>
2020-06-30 18:36:44 -0400

    Main: 4 variables construidas a partir de 'LibrosEscolares' + método imprimir()

--- src/Main.kt
+++ src/Main.kt
@@ -6,13 +6,61 @@ Los datos que deben almacenar son:
 * Año publicación
 * Editorial
 * Cantidad de páginas
-* PrecioAutor del libro
+* Precio
+* Autor del libro
 * Tipo de libro (D: Digital / I: Impreso)
 */
 
 // 5.
 fun main() {
 
+// 6.
+    var libro1 = LibrosEscolares(
+        "Hitchhiker's Guide to the Galaxy",
+        "9780010-10",
+        "1978",
+        "Ballantine Books",
+        224,
+        2500,
+        "Douglas Adams",
+        "D"
+    )
+    var libro2 = LibrosEscolares(
+        "Crónica de una muerte anunciada",
+        "9780010-20",
+        "1981",
+        "Vintage Books",
+        128,
+        9000,
+        "Gabriel García Marquez",
+        "D"
+    )
+    var libro3 = LibrosEscolares(
+        "Eva Luna",
+        "9780010-30",
+        "1987",
+        "Atria",
+        368,
+        6700,
+        "Isabel Allende",
+        "D"
+    )
+    var libro4 = LibrosEscolares(
+        "Minima Moralia: Reflexiones desde la vida dañada",
+        "9780010-40",
+        "1951",
+        "Ediciones Akal",
+        278,
+        5000,
+        "Theodor W. Adorno",
+        "D"
+    )
+
+// 7.
+    imprimir(libro1)
+    imprimir(libro2)
+    imprimir(libro3)
+    imprimir(libro4)
 }
 
 // 3.
@@ -31,6 +79,6 @@ fun imprimir(libro: LibrosEscolares) { // por cada `libros` en `LibrosEscolares`
                 "Número de páginas: ${libro.cantPaginas}\n" +
                 "Precio: ${precioFormateado(libro.precio)}\n" +
                 "Autor: ${libro.autorLibro}\n" +
-                "Formato: ${libro.formatoDeLibro}\n"
+                "Formato: ${libro.formatoDeLibro}\n" + "\n"
     )
 }
