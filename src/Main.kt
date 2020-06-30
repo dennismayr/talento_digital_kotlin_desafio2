17ed3cb9bfcc6aa719faeb722ab69509e0e291fa
Dennis Mayr <dmayr.dev@gmail.com>
2020-06-30 16:37:24 -0400

    Corregido espaciado e interlineado de comentario/instrucción inicial

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,13 @@
 /*
-Los datos que deben almacenar son:Nombre del libro.ISBN.Año publicaciónEditorialCantidad de páginasPrecioAutor del libroTipo de libro (D: Digital / I: Impreso)Este sistema debe tener una funcionalidad que permita devolver el precio con el signo $ en cualquier parte del proyecto. Además, debe contar con una función que imprima todos la información del libro.Para terminar, se solicita crear la función “main” y declarar 4 variables de tipo “LibrosEscolares”,donde 3 de estas variables deben ser mutables y una variable debe ser inmutable.Imprimir las 4 variables llamando a la función que imprime toda la información.
+Los datos que deben almacenar son:
+
+* Nombre del libro
+* ISBN
+* Año publicación
+* Editorial
+* Cantidad de páginas
+* PrecioAutor del libro
+* Tipo de libro (D: Digital / I: Impreso)
 */
 
 // 5.
